d4340045d75e9db698ef2261ef490bd18b99f284
DenisKostyukov <68915197+DenisKostyukov@users.noreply.github.com>
2020-07-28 20:11:25 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+  $(".service-card").hover(
+        function () {
+    $(this).find('img').attr('src', 'img/smartphone2.png');
+        },
+        function () {
+    $(this).find('img').attr('src', 'img/smartphone.png');
+        });
+        
