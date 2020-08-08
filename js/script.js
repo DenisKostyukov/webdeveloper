225de48141005d413d46349aa8a5e7776c807fbf
DenisKostyukov <68915197+DenisKostyukov@users.noreply.github.com>
2020-08-08 17:57:23 +0300

    burg

--- js/script.js
+++ js/script.js
@@ -7,18 +7,5 @@
     $(this).find('img').attr('src', 'img/smartphone.png');
         });
 
-
-        $(function () {
-            var link = $('.m-menu-link');
-            var close = $('.close-menu');
-            var menu = $('.m-menu');
-            link.on('click', function (event) {
-                event.preventDefault();
-                menu.toggleClass('m-menu__active');
-            });
-            close.on('click', function (event) {
-                event.preventDefault();
-                menu.toggleClass('m-menu__active');
-            });
-        });
+        
         
